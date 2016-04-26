91c54c6908e4549bcc264ffc2678d9d4098b21ad
Errol LeBoydre <errol@wyldlynx.com.au>
2016-04-26 21:17:32 +1000

    Update

---
+++ caApplyACL/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HP.HPTRIM.SDK;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace caApplyACL
+{
+    class ACL
+    {
+        public string EcmClass { get; set; }
+        public string RMlocation { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ACL> lstAcl = new List<ACL>();
+            using (SqlConnection con = new SqlConnection("Data Source=Cl1025;Initial Catalog=StagingMove;Integrated Security=True"))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand("SELECT [ECMClass],[EddieAcl] FROM [StagingMove].[dbo].[SetAcl] where Run = 1", con))
+                {
+                    //DataTable dt = new DataTable();
+                    //SqlDataAdapter da = new SqlDataAdapter();
+                    //da.SelectCommand = command;
+                    //da.Fill(dt);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ACL acl = new ACL();
+                            acl.EcmClass = reader.GetString(0).Trim();
+                            acl.RMlocation = reader.GetString(1).Trim();
+                            lstAcl.Add(acl);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("What is the origin uri?");
+            int iOuri = Convert.ToInt32(Console.ReadLine());
+
+            foreach (ACL acl in lstAcl)
+            {
+                try
+                {
+
+                    using (Database db = new Database())
+                    {
+                        //Console.WriteLine("Enter ECM class name");
+                        TrimMainObjectSearch sObj = new TrimMainObjectSearch(db, BaseObjectTypes.Record);
+                        //ECMSTDClassName:"Confidential Transition" and type:16
+                        //string str = @""+Console.ReadLine()+"";
+                        //sObj.SetSearchString("ECMSTDClassName:\"" + acl.EcmClass + "\" and type:16 and updated>9/03/2016");
+                        sObj.SetSearchString("ECMSTDClassName:\"" + acl.EcmClass + "\" and type:16 and originRun:"+iOuri);
+                        Console.WriteLine(sObj.SearchString);
+                        Console.WriteLine("Record count: " + sObj.FastCount.ToString());
+                        
+                        if (sObj.FastCount > 0)
+                        {
+                            FieldDefinition fdDocDate = new FieldDefinition(db, 85);
+                            LocationList lstLoc = new LocationList();
+                            Location l = new Location(db, acl.RMlocation);
+                            lstLoc.Add(l);
+                            foreach (Record r in sObj)
+                            {
+                                if(r.DateRegistered<=r.DateReceived)
+                                {
+                                    DateTime tdtDateReceived = r.DateReceived;
+                                    r.DateRegistered = (TrimDateTime)tdtDateReceived.AddHours(1);
+                                }
+                                r.Save();
+                                //TrimDateTime rmDt = (TrimDateTime)r.GetFieldValue(fdDocDate).ToDotNetObject();
+                                
+                                //r.DateReceived = rmDt;
+                                //r.DateCreated = rmDt;
+                                //r.Save();
+
+                                //
+                                TrimAccessControlList lstacl = r.AccessControlList;
+                                lstacl.set_AccessLocations((int)RecordAccess.ViewRecord, lstLoc);
+                                lstacl.set_AccessLocations((int)RecordAccess.ViewDocument, lstLoc);
+                                r.AccessControlList = lstacl;
+                                try
+                                {
+                                    r.Save();
+                                    Console.WriteLine("ACL applied to " + r.Number);
+                                }
+                                catch (Exception exp)
+                                {
+                                    Console.WriteLine("ACL failed, Error: " + exp.Message.ToString());
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine("Error: " + exp.Message.ToString());
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- caCheckFileLocation/Program.cs
+++ caCheckFileLocation/Program.cs
@@ -12,143 +12,162 @@ namespace caCheckFileLocation
     {
         static void Main(string[] args)
         {
+            string strmessage = null;
             TrimApplication.Initialize();
             using (Database db = new Database())
             {
                 //updated:18/04/2016 07:30:00 to 18/04/2016 09:00:00 and electronic
-                TrimMainObjectSearch objS = new TrimMainObjectSearch(db, BaseObjectTypes.Record);
-                objS.SetSearchString("updated:18/04/2016 07:30:00 to 18/04/2016 09:00:00 and electronic");
-                foreach(Record rec in objS)
-                {
-                    string[] split = rec.DocumentDetails.Split(':');
-                    string FilePath = split.Last().Replace("+", "\\");
-                    //Console.WriteLine("Location: " + rec.EStore.Path + FilePath);
-
-                    if (rec.EStore.Uri == 1)
-                    {
-                        if (File.Exists(rec.EStore.Path + FilePath))
-                        {
-                            //Console.WriteLine("doc exists in Repos1: " + rec.EStore.Path + FilePath);
-                        }
-                        else
-                        {
-                            Console.WriteLine("doc does not exist in Repos1: "+rec.Number + " - "  + rec.EStore.Path + FilePath);
-                            ElectronicStore es3 = new ElectronicStore(db, 503);
-                            if (File.Exists(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath))
-                            {
-                                Console.WriteLine("doc exists in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
-
-                                string[] strfilename = FilePath.Split('\\');
-
-                                try
-                                {
-                                    //File.Copy(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath, rec.EStore.Path + FilePath);
-                                    Console.WriteLine("Move doc here:");
-                                }
-                                catch (Exception exp)
-                                {
-                                    Console.WriteLine("Error: " + exp.Message.ToString());
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("doc does not exist in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
-                            }
-
-
-                            //Console.WriteLine("doc does not exist in Repos1: " + es1.Path + FilePath);
-                        }
-                    }
-                }
-
-
-
-
-
-
-
-
-
-
-
-                //Console.WriteLine("Enter record uri to check");
-                //long lUri = Convert.ToInt64(Console.ReadLine());
-                //Record rec = (Record)db.FindTrimObjectByUri(BaseObjectTypes.Record, lUri);
-                //if(rec!=null)
+                //TrimMainObjectSearch objS = new TrimMainObjectSearch(db, BaseObjectTypes.Record);
+                //objS.SetSearchString("updated:17/04/2016 05:30:00 to 21/04/2016 05:00:00 and electronic");
+                //foreach (Record rec in objS)
                 //{
-                //    Console.WriteLine("Found Record: " + rec.Number);
-                //    if(rec.IsElectronic)
+                //    string FilePath1 = null;
+                //    var f = rec.DocumentDetails;
+                //    if (f.Contains("Finalized"))
                 //    {
-                //        Console.WriteLine("Record docstore location: " + rec.DocumentDetails);
-                //        string[] split = rec.DocumentDetails.Split(':');
-                //        string FilePath = split.Last().Replace("+", "\\");
-                //        Console.WriteLine("Location: " + rec.EStore.Path + FilePath);
-
-                //        if(rec.EStore.Uri==1)
-                //        {
-                //            if (File.Exists(rec.EStore.Path + FilePath))
-                //            {
-                //                Console.WriteLine("doc exists in Repos1: " + rec.EStore.Path + FilePath);
-                //            }
-                //            else
-                //            {
-                //                ElectronicStore es3 = new ElectronicStore(db, 503);
-                //                if (File.Exists(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath))
-                //                {
-                //                    Console.WriteLine("doc exists in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
-
-                //                    string[] strfilename = FilePath.Split('\\');
-
-                //                    try
-                //                    {
-                //                        File.Copy(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath, rec.EStore.Path + FilePath);
-                //                    }
-                //                    catch(Exception exp)
-                //                    {
-                //                        Console.WriteLine("Error: " + exp.Message.ToString());
-                //                    }
-                //                }
-                //                else
-                //                {
-                //                    Console.WriteLine("doc does not exist in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
-                //                }
-
-
-                //                //Console.WriteLine("doc does not exist in Repos1: " + es1.Path + FilePath);
-                //            }
-                //        }
-
-
-                //        //ElectronicStore es3 = new ElectronicStore(db, 503);
-                //        //if (File.Exists(@"\\C1\DocStore\REPOS3\SP\1\"+FilePath))
-                //        //{
-                //        //    Console.WriteLine("doc exists in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
-                //        //}
-                //        //else
-                //        //{
-                //        //    Console.WriteLine("doc does not exist in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
-                //        //}
-                //        //ElectronicStore es1 = new ElectronicStore(db, 1);
-                //        //if (File.Exists(es1.Path + FilePath))
-                //        //{
-                //        //    Console.WriteLine("doc exists in Repos1: " + es1.Path + FilePath);
-                //        //}
-                //        //else
-                //        //{
-                //        //    Console.WriteLine("doc does not exist in Repos1: " + es1.Path + FilePath);
-                //        //}
+                //        FilePath1 = f.Substring(0, f.Length - 37);
                 //    }
                 //    else
                 //    {
-                //        Console.WriteLine("Record has no elec doc");
+                //        FilePath1 = f;
+                //    }
+                //    string[] k = FilePath1.Split(':');
+                //    string FilePath = k.Last().Replace("+", "\\").Trim();
+
+                //    string strFound = @"\\C1\DocStore\REPOS3\SP\1\" + FilePath;
+                //    if (rec.EStore.Uri == 1)
+                //    {
+                //        if (!File.Exists(rec.EStore.Path + FilePath))
+                //        {
+                //            Console.WriteLine("doc does not exist in Repos1: " + rec.Number + " - " + rec.EStore.Path + FilePath);
+                //            strmessage = strmessage + "Doc does not exist in Repos1: " + rec.Number + "(" + rec.Uri.ToString() + ") - " + rec.EStore.Path + FilePath;
+                //            ElectronicStore es3 = new ElectronicStore(db, 503);
+                //            if (File.Exists(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath))
+                //            {
+
+                //                Console.WriteLine("doc exists in Repos3: " + strFound);
+                //                strmessage = strmessage + ", Found doc in " + strFound;
+                //                string[] strfilename = FilePath.Split('\\');
+
+                //                try
+                //                {
+                //                    File.Copy(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath, rec.EStore.Path + FilePath);
+                //                    Console.WriteLine("Successfully moved document to right location");
+
+
+                //                    //File.Copy(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath, rec.EStore.Path + FilePath);
+                //                    //Console.WriteLine("Move doc here:");
+                //                    strmessage = strmessage + " - Successfully moved doc";
+
+                //                }
+                //                catch (Exception exp)
+                //                {
+                //                    Console.WriteLine("Error: " + exp.Message.ToString());
+                //                    strmessage = strmessage + " - Doc could not be moved, error: "+exp.Message.ToString();
+                //                }
+                //            }
+                //            else
+                //            {
+                //                Console.WriteLine("doc does not exist in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
+                //                strmessage = strmessage + ", Did not find doc in " + strFound;
+                //            }
+
+
+                //            //Console.WriteLine("doc does not exist in Repos1: " + es1.Path + FilePath);
+                //            strmessage = strmessage + Environment.NewLine;
+                //        }
+
                 //    }
                 //}
-                //else
-                //{
-                //    Console.WriteLine("Record not found");
-                //}
+
+                //Loggitt(strmessage, "Missing Docs review");
+
+
+
+                Console.WriteLine("Enter record uri to check");
+                long lUri = Convert.ToInt64(Console.ReadLine());
+                Record rec = (Record)db.FindTrimObjectByUri(BaseObjectTypes.Record, lUri);
+                if (rec != null)
+                {
+                    Console.WriteLine("Found Record: " + rec.Number);
+                    if (rec.IsElectronic)
+                    {
+                        Console.WriteLine("Record docstore location: " + rec.DocumentDetails);
+                        string[] split = rec.DocumentDetails.Split(':');
+                        string FilePath = split.Last().Replace("+", "\\");
+                        Console.WriteLine("Location: " + rec.EStore.Path + FilePath);
+
+                        //if (rec.EStore.Uri == 1)
+                        //{
+                            if (File.Exists(rec.EStore.Path + FilePath))
+                            {
+                                Console.WriteLine("doc exists in Repos1: " + rec.EStore.Path + FilePath);
+                            }
+                            else
+                            {
+                                ElectronicStore es3 = new ElectronicStore(db, 503);
+                                if (File.Exists(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath))
+                                {
+                                    Console.WriteLine("doc exists in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
+
+                                    string[] strfilename = FilePath.Split('\\');
+
+                                    try
+                                    {
+                                        File.Copy(@"\\C1\DocStore\REPOS3\SP\1\" + FilePath, rec.EStore.Path + FilePath);
+                                        Console.WriteLine("Successfully moved document to right location");
+
+                                    }
+                                    catch (Exception exp)
+                                    {
+                                        Console.WriteLine("Doc copy Error: " + exp.Message.ToString());
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("doc does not exist in Repos3: " + @"\\C1\DocStore\REPOS3\SP\1\" + FilePath);
+                                }
+
+
+                                //Console.WriteLine("doc does not exist in Repos1: " + es1.Path + FilePath);
+                            }
+                        //}
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Record has no elec doc");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Record not found");
+                }
             }
+            Console.WriteLine("Finished, press any key to close.");
             Console.ReadLine();
+        }
+        public static void Loggitt(string msg, string title)
+        {
+            if (!(System.IO.Directory.Exists(@"C:\Temp\MissingDocs\")))
+            {
+                System.IO.Directory.CreateDirectory(@"C:\Temp\MissingDocs\");
+            }
+            FileStream fs = new FileStream(@"C:\Temp\MissingDocs\log_" + DateTime.Today.ToShortDateString().Replace("/", "") + ".txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter s = new StreamWriter(fs);
+            s.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream(@"C:\Temp\MissingDocs\log_" + DateTime.Today.ToShortDateString().Replace("/", "") + ".txt", FileMode.Append, FileAccess.Write);
+            StreamWriter s1 = new StreamWriter(fs1);
+            s1.Write("Title: " + title + Environment.NewLine);
+            s1.Write("Message: " + msg + Environment.NewLine);
+            //s1.Write("StackTrace: " + stkTrace + Environment.NewLine);
+            s1.Write("Date/Time: " + DateTime.Now.ToString() + Environment.NewLine);
+            s1.Write
+("============================================" + Environment.NewLine);
+            s1.Close();
+            fs1.Close();
         }
     }
 }
